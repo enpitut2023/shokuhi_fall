9b450e44977c01d6b1ddcd331dc78b24cff1f8fd
HitoTei <s2313541@u.tsukuba.ac.jp>
2023-12-08 17:17:16 +0900

    improve ui

--- lib/ui/shop_detail/shop_detail.dart
+++ lib/ui/shop_detail/shop_detail.dart
@@ -33,108 +33,125 @@ class ShopDetail extends ConsumerWidget {
       merchDetailListProvider(shopId: shop.id),
     );
 
+    final size = MediaQuery.of(context).size;
+    final aspect = size.width / size.height;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(shop.name),
       ),
-      body: Row(
-        children: [
-          Expanded(
-            child: ListView(
+      body: aspect > 1
+          ? Row(children: [
+              Expanded(
+                child: ListView(
+                  children: _shopDetailListChildren(
+                      merchDetailList, dayOfWeek, context),
+                ),
+              ),
+              Expanded(child: MapWidget(shop.longitude, shop.latitude)),
+            ])
+          : ListView(
               children: [
-                ListTile(
-                  title: const Text('住所'),
-                  subtitle: Text(shop.address),
+                SizedBox.fromSize(
+                  size: size / 2,
+                  child: MapWidget(shop.longitude, shop.latitude),
                 ),
-                ListTile(
-                  title: const Text('電話番号'),
-                  subtitle: Text(shop.tel),
-                ),
-                const ListTile(title: Text('営業時間')),
-                for (final dow in [
-                  "Monday",
-                  "Tuesday",
-                  "Wednesday",
-                  "Thursday",
-                  "Friday",
-                  "Saturday",
-                  "Sunday"
-                ])
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 80,
-                          child: Text(
-                            '$dow: ',
-                            style: TextStyle(
-                              fontWeight: dayOfWeek == dow
-                                  ? FontWeight.bold
-                                  : FontWeight.normal,
-                              color:
-                                  dayOfWeek == dow ? Colors.black : Colors.grey,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          shop.open[dow] == '00:00' &&
-                                  shop.close[dow] == '00:00'
-                              ? '24時間営業'
-                              : '${shop.open[dow]} - ${shop.close[dow] == '00:00' ? '24:00' : shop.close[dow]}',
-                          style: TextStyle(
-                            fontWeight: dayOfWeek == dow
-                                ? FontWeight.bold
-                                : FontWeight.normal,
-                            color:
-                                dayOfWeek == dow ? Colors.black : Colors.grey,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                if (shop.comments.isNotEmpty)
-                  const ListTile(
-                    title: Text('コメント'),
-                  ),
-                for (final comment in shop.comments.entries)
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16.0),
-                    child: Text('- ${comment.value}'),
-                  ),
-                const ListTile(
-                  title: Text('商品一覧'),
-                ),
-                const Divider(),
-                AsyncValueWidget(
-                  value: merchDetailList,
-                  builder: (data) => Column(
-                    children: [
-                      for (final merch in data)
-                        MerchDetailTile(
-                          merch,
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => PostedMerchList(
-                                  shopId: shop.id,
-                                  merchDetailId: merch.id,
-                                  merchName: merch.name,
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                    ],
-                  ),
-                ),
+                for (final child in _shopDetailListChildren(
+                    merchDetailList, dayOfWeek, context))
+                  child,
               ],
             ),
-          ),
-          Expanded(child: MapWidget(shop.longitude, shop.latitude)),
-        ],
-      ),
     );
+  }
+
+  List<Widget> _shopDetailListChildren(
+    AsyncValue<List<MerchDetail>> merchDetailList,
+    String dayOfWeek,
+    BuildContext context,
+  ) {
+    return [
+      ListTile(
+        title: const Text('住所'),
+        subtitle: Text(shop.address),
+      ),
+      ListTile(
+        title: const Text('電話番号'),
+        subtitle: Text(shop.tel),
+      ),
+      const ListTile(title: Text('営業時間')),
+      for (final dow in [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ])
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 80,
+                child: Text(
+                  '$dow: ',
+                  style: TextStyle(
+                    fontWeight:
+                        dayOfWeek == dow ? FontWeight.bold : FontWeight.normal,
+                    color: dayOfWeek == dow ? Colors.black : Colors.grey,
+                  ),
+                ),
+              ),
+              Text(
+                shop.open[dow] == '00:00' && shop.close[dow] == '00:00'
+                    ? '24時間営業'
+                    : '${shop.open[dow]} - ${shop.close[dow] == '00:00' ? '24:00' : shop.close[dow]}',
+                style: TextStyle(
+                  fontWeight:
+                      dayOfWeek == dow ? FontWeight.bold : FontWeight.normal,
+                  color: dayOfWeek == dow ? Colors.black : Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
+      if (shop.comments.isNotEmpty)
+        const ListTile(
+          title: Text('コメント'),
+        ),
+      for (final comment in shop.comments.entries)
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0),
+          child: Text('- ${comment.value}'),
+        ),
+      const ListTile(
+        title: Text('商品一覧'),
+      ),
+      const Divider(),
+      AsyncValueWidget(
+        value: merchDetailList,
+        builder: (data) => Column(
+          children: [
+            for (final merch in data)
+              MerchDetailTile(
+                merch,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PostedMerchList(
+                        shopId: shop.id,
+                        merchDetailId: merch.id,
+                        merchName: merch.name,
+                      ),
+                    ),
+                  );
+                },
+              ),
+          ],
+        ),
+      ),
+    ];
   }
 }
