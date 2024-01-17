2429a299dc2ffc381c345fff046896b687f18f3f
taiyou-sun <sun.bigroad1153@gmail.com>
2024-01-17 14:31:27 +0900

    商品選択画面のヘルプ機能実装

--- lib/ui/home/home.dart
+++ lib/ui/home/home.dart
@@ -30,7 +30,13 @@ class Home extends ConsumerWidget {
       appBar: AppBar(
         title: (index == 0) ? const MerchListTitle() : const PostPageTitle(),
         actions: [
-          if (index == 0) const MerchListAction(),
+          if (index == 0)
+            IconButton(
+              icon: const Icon(Icons.help),
+              color: Colors.purple.withOpacity(0.6),
+              onPressed: () {
+                _showDialog(context);
+              },), if (index == 0) const MerchListAction(),
           if (index == 1)
             OutlinedButton(
               onPressed: () {
@@ -60,6 +66,46 @@ class Home extends ConsumerWidget {
           notifier.set(index);
         },
       ),
+    );
+  }
+  Future<void> _showDialog(BuildContext context) async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('ヘルプ'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('商品を選んで検索する手順を説明します。'),
+                const Text(''),
+                const Text('① まずは商品一覧から購入したい商品を選んでください。'),
+                Image.asset(
+                  'images/merch_help1.jpeg',
+                ),
+                const Text(''),
+                const Text('② 商品をクリックすると下のような画面が表示されるので、その商品の購入したい量を入力してください。'),
+                Image.asset(
+                  'images/merch_help2.jpeg',
+                ),
+                const Text(''),
+                const Text('③ 最後に、右下の「お店を探す」ボタンをクリックすることでお店ごとの商品の価格を調べることができます。'),
+                Image.asset(
+                  'images/merch_help3.jpeg',
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('閉じる'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
