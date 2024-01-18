439980f73c26d475cbb5f9787f949eefb80240de
Taiki-Kobasako <taiki5835@gmail.com>
2024-01-19 01:13:58 +0900

    fix:画像

--- lib/ui/home/help/help.dart
+++ lib/ui/home/help/help.dart
@@ -68,7 +68,7 @@ class PostHelpDialog extends StatelessWidget {
             const Text(''),
             const Text('③ 最後に、右下の「投稿」ボタンをクリックすることで投稿することが出来ます。'),
             Image.asset(
-              'images/post_help2.jpeg',
+              'images/post_help3.jpeg',
             ),
           ],
         ),
